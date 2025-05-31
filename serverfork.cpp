acf37e3d83a1fdcd30b25eba00ec69b507a5cf50
Surya <suko19@student.bth.se>
2025-05-31 12:00:36 +0200

    fork begin

--- serverfork.cpp
+++ serverfork.cpp
@@ -6,7 +6,43 @@
 
 /* You will to add includes here */
 
+static const char *STATUS_200 =
+    "HTTP/1.1 200 OK\r\nConnection: close\r\n";
+static const char *STATUS_404 =
+    "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Length: 0\r\n\r\n";
+static const char *STATUS_405 =
+    "HTTP/1.1 405 Method Not Allowed\r\nAllow: GET, HEAD\r\nConnection: close\r\nContent-Length: 0\r\n\r\n";
+static const char *STATUS_400 =
+    "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 0\r\n\r\n";
+    
+static bool safe_path(const std::string &u) {
+    /* Reject: more than one '/' after the leading slash, or any “..” */
+    if (u.find("..") != std::string::npos) return false;
+    size_t slash_cnt = std::count(u.begin(), u.end(), '/');
+    return slash_cnt <= 1;
+}
 
+static std::pair<std::string,std::string> parse_req(const char *buf) {
+    std::istringstream iss(buf);
+    std::string method, url, version;
+    iss >> method >> url >> version;
+    return {method, url};
+}
+
+static void send_file(int client_fd, int file_fd, size_t length, bool body) {
+    std::ostringstream hdr;
+    hdr << STATUS_200
+        << "Content-Length: " << length << "\r\n\r\n";
+    std::string h = hdr.str();
+    send(client_fd, h.c_str(), h.size(), 0);
+
+    if (body && length) {
+        off_t offset = 0;
+        while (offset < static_cast<off_t>(length))
+            offset += sendfile(client_fd, file_fd, &offset,
+                               length - offset);
+    }
+}
 
 using namespace std;
 
